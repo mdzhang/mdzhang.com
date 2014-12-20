0c7190e525f67faea78bc066b47a0b49826eca84
mdzhang <zhang.michelle.d@gmail.com>
2014-12-20 02:54:57 -0500

    Added 'About me' content

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // smooth scroll to part of page linked to by an anchor link
     $('.anchor-link').click(function(){
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top - $('#section-topbar').height()
+            scrollTop: $( $(this).attr('href') ).offset().top - $('#section-topbar').height() / 2
         }, 'slow');
         return false;
     });
