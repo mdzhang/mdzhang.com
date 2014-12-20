09f8633dc2171b922cad85752dee9bb352f0144e
mdzhang <zhang.michelle.d@gmail.com>
2014-12-20 00:42:45 -0500

    Added smooth scrolling on anchor link click
    - Also switched email icon from '@' to a envelope outline
    - Linked icon to top of page (top of section-banner)

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    // smooth scroll to part of page linked to by an anchor link
+    $('.anchor-link').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - $('#section-topbar').height()
+        }, 'slow');
+        return false;
+    });
+
+});
