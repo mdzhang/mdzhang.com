103d7c9731fc11b18705505bc47bbcc0071a992b
Michelle Zhang <zhang.michelle.d@gmail.com>
2015-07-05 17:08:19 -0400

    Updated deploy script

---
+++ Makefile
@@ -0,0 +1,16 @@
+S3_BUCKET=mdzhang.com
+
+# Sync current working directory with s3.
+deploy:
+	s3cmd sync --delete-removed --acl-public --dry-run \
+		--exclude '*' \
+		--include 'js/*' \
+		--include 'css/*' \
+		--include 'images/*' \
+		--include 'files/*' \
+		--include 'index.html' \
+		./ s3://$(S3_BUCKET)/
+
+.PHONY: clean
+clean:
+	find . -name "*~" -exec rm -rf {} \;
--- build/deploy.sh
+++ build/deploy.sh
@@ -4,5 +4,11 @@
 S3_BUCKET=mdzhang.com
 
 # Sync current working directory with s3.
-#
-s3cmd sync --delete-removed --acl-public --exclude '.git/*' ./ s3://$S3_BUCKET/
+s3cmd sync --delete-removed --acl-public --dry-run \
+  --exclude '*/*' \
+  --include 'js/*' \
+  --include 'css/*' \
+  --include 'images/*' \
+  --include 'files/*' \
+  --include 'index.html' \
+  ./ s3://$S3_BUCKET/
