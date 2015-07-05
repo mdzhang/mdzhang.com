ed63da5f6ed149822da16c939c2492eb5b68e98b
Michelle Zhang <zhang.michelle.d@gmail.com>
2015-07-05 17:08:19 -0400

    Updated deploy script

--- build/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# See http://s3tools.org/s3cmd
-
-S3_BUCKET=mdzhang.com
-
-# Sync current working directory with s3.
-#
-s3cmd sync --delete-removed --acl-public --exclude '.git/*' ./ s3://$S3_BUCKET/
