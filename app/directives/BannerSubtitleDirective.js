5ccfdcf1fe0f5a6dc41896426e88f28dd7ca6180
Michelle Zhang <zhang.michelle.d@gmail.com>
2015-07-20 08:22:05 -0400

    Social media icon zoom in/out

--- app/directives/BannerSubtitleDirective.js
+++ app/directives/BannerSubtitleDirective.js
@@ -31,19 +31,25 @@
 
         $q.resolve()
           .then(function() {
+            // Angular seems to not clear the binded value when the subtitle element is slid out,
+            // so hide the whole directive, so only the subtitle can be seen sliding out to the left.
+            // TODO: This is a hack, and its cause is not yet fully understood.
             $element.css({ visibility: 'hidden' });
             angular.element(_this.subtitleElement).addClass('slideOutToLeft');
 
+            // Wait for the subtitle to finish sliding out to the left before we remove the class
+            // so we can slide it out anew next time.
             return $timeout(function() {
               angular.element(_this.subtitleElement).removeClass('slideOutToLeft');
             }, 1000);
           })
           .then(function() {
+            // Re-reveal the directive. See above for why we hid it in the first place.
+            $element.css({ visibility: 'visible' });
             // NB: We _must_ update the scope subtitle _after_ we start the animation,
             //     else the user will first see a blink to the next subtitle, and then see
             //     the next subtitle animated into the screen, since we can change the subtitle
             //     value near instantaneously, but the animation takes 1 second +.
-            $element.css({ visibility: 'visible' });
             angular.element(_this.subtitleElement).addClass('slideInFromRight');
             $scope.subtitle = nextSubtitle;
 
@@ -54,6 +60,7 @@
             }, 2000);
           })
           .then(function() {
+            // Wait before looping the next subtitle in.
             return $timeout(function() {
               _infinitelyLoopSubtitles();
             }, _this.interval);
