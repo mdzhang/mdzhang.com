17fcb4eceef7baf93c4e8e3fd6e74a8b01bbda27
Michelle D Zhang <zhang.michelle.d@gmail.com>
2016-03-05 12:26:53 -0500

    Remove unused css selectors and minify html to speed up website load time

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,18 @@ module.exports = function(grunt) {
   bowercss = _.map(bowercss, rewrite);
 
   alljs = bowerjs.concat('public/build/tmp/scripts.js');
-  // our style sheet has @imports, which must be at the top of the file for cssmin to handle properly
+  // our style sheet has @imports, which must be at the top of the file
+  // for cssmin to handle properly
   allcss = ['public/build/tmp/styles.css'].concat(bowercss);
 
   // Project configuration.
   grunt.initConfig({
     // build html files from jade templates
     jade: {
+      options: {
+        // we need to keep the whitespace so that htmlprocess can work properly
+        pretty: true
+      },
       compile: {
         files: [
           {
@@ -105,14 +110,46 @@ module.exports = function(grunt) {
       },
       css: {
         src: allcss,
-        dest: 'public/build/tmp/styles.css'
+        dest: 'public/build/css/styles.css'
+      }
+    },
+    // removed unused css selectors
+    uncss: {
+      dist: {
+        files: {
+          'public/build/tmp/tidy.css': ['public/build/index.html', 'public/build/error.html']
+        }
       }
     },
     // minify css files
     cssmin: {
       target: {
         files: {
-          'public/build/css/styles.min.css': 'public/build/tmp/styles.css'
+          'public/build/css/tidy.min.css': 'public/build/tmp/tidy.css'
+        }
+      }
+    },
+    // modify html files to use build resources
+    processhtml: {
+      dist: {
+        files: {
+          'public/build/index.html': ['public/build/index.html'],
+          'public/build/error.html': ['public/build/error.html']
+        }
+      },
+    },
+    // minify html files
+    htmlmin: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true,
+        minifyJS: true,
+        processScripts: ['application/ld+json']
+      },
+      dist: {
+        files: {
+          'public/build/index.html': ['public/build/index.html'],
+          'public/build/error.html': ['public/build/error.html']
         }
       }
     },
@@ -158,8 +195,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-jscs");
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-uncss');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -167,8 +207,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-coffee');
 
   // Default task(s).
-  grunt.registerTask('buildhtml', ['jade']);
-  grunt.registerTask('buildcss', ['sass', 'postcss', 'concat:css', 'cssmin']);
+  grunt.registerTask('buildhtml', ['jade', 'processhtml', 'htmlmin']);
+  grunt.registerTask('buildcss', ['sass', 'postcss', 'concat:css', 'uncss', 'cssmin']);
   grunt.registerTask('buildjs', ['coffee', 'jshint', 'jscs', 'concat:js', 'uglify']);
   grunt.registerTask('build', ['buildhtml', 'buildcss', 'buildjs', 'clean']);
   grunt.registerTask('default', ['build', 'watch']);
