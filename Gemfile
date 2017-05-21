0a076314bad54180e118bf69d20823680d0e3d4c
Michelle D Zhang <zhang.michelle.d@gmail.com>
2017-05-21 12:25:25 -0400

    Switch to airbnb eslint config

--- Gemfile
+++ Gemfile
@@ -1,19 +1,22 @@
 source 'https://rubygems.org'
 ruby File.read('.ruby-version').chomp if File.exist?('.ruby-version')
 
+# See https://github.com/middleman/middleman/issues/2087
+gem 'haml', '~> 4.0'
+
+# See http://stackoverflow.com/a/41456412/2699835
+gem 'json', git: 'https://github.com/flori/json', branch: 'v1.8'
+
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-autoprefixer'
 gem 'middleman-blog'
 gem 'middleman-jquery'
 gem 'middleman-s3_sync'
 gem 'middleman-sprockets'
+gem 'middleman-syntax'
 gem 'mime-types'
 
-gem 'middleman-syntax'
 gem 'redcarpet'
-
-# See http://stackoverflow.com/a/41456412/2699835
-gem 'json', git: 'https://github.com/flori/json', branch: 'v1.8'
 
 group :build do
   gem 'middleman-minify-html'
@@ -30,12 +33,9 @@ group :development do
   gem 'scss_lint', require: false
 end
 
-group :test do
-  gem 'capybara'
-  gem 'rspec'
-end
-
 group :test, :development do
+  gem 'capybara'
   gem 'middleman-livereload'
   gem 'middleman-pry'
+  gem 'rspec'
 end
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,71 +1,62 @@
 //= require jquery
-(function() {
-  $(function() {
-    var tooltipTargetClick, tooltipTargetHover, tooltipTargetHoverOut;
+$(() => {
+  const tooltipTargetHover = (event) => {
+    const dot = $(event.target);
+    const selectedLi = dot.parent();
+    const ul = selectedLi.parent();
+    const currentLi = ul.find('.current');
+    if (selectedLi.is(currentLi)) {
+      return;
+    }
+    selectedLi.css('z-index', 1000);
+    selectedLi.find('.tooltip').css({
+      visibility: 'visible',
+      opacity: 1,
+    });
+    if (!currentLi) {
+      return;
+    }
+    currentLi.find('.tooltip').css('opacity', 0.5);
+  };
 
-    tooltipTargetHover = function(event) {
-      var currentLi, dot, selectedLi, ul;
-      dot = $(event.target);
-      selectedLi = dot.parent();
-      ul = selectedLi.parent();
-      currentLi = ul.find('.current');
-      if (selectedLi.is(currentLi)) {
-        return;
-      }
-      selectedLi.css('z-index', 1000);
-      selectedLi.find('.tooltip').css({
-        visibility: 'visible',
-        opacity: 1
-      });
-      if (!currentLi) {
-        return;
-      }
-      currentLi.find('.tooltip').css('opacity', 0.5);
-    };
+  const tooltipTargetHoverOut = (event) => {
+    const dot = $(event.target);
+    const selectedLi = dot.parent();
+    const ul = selectedLi.parent();
+    const currentLi = ul.find('.current');
+    selectedLi.css('z-index', 999);
+    if (selectedLi.is(currentLi)) {
+      return;
+    }
+    selectedLi.find('.tooltip').css({
+      visibility: 'hidden',
+      opacity: 0,
+    });
+    ul.find('.current').find('.tooltip').css('opacity', 1);
+  };
 
-    tooltipTargetHoverOut = function(event) {
-      var currentLi, dot, selectedLi, ul;
-      dot = $(event.target);
-      selectedLi = dot.parent();
-      ul = selectedLi.parent();
-      currentLi = ul.find('.current');
-      selectedLi.css('z-index', 999);
-      if (selectedLi.is(currentLi)) {
-        return;
-      }
-      selectedLi.find('.tooltip').css({
+  const tooltipTargetClick = (event) => {
+    const dot = $(event.target);
+    const selectedLi = dot.parent();
+    const ul = selectedLi.parent();
+    const currentLi = ul.find('.current');
+    if (selectedLi.is(currentLi)) {
+      return;
+    }
+    if (currentLi) {
+      currentLi.removeClass('current');
+      currentLi.find('.tooltip').css({
         visibility: 'hidden',
-        opacity: 0
+        opacity: 0,
       });
-      ul.find('.current').find('.tooltip').css('opacity', 1);
-    };
+    }
+    selectedLi.addClass('current');
+    if (dot.attr('href')) {
+      selectedLi.removeClass('current');
+    }
+  };
 
-    tooltipTargetClick = function(event) {
-      var currentLi, dot, selectedLi, ul;
-      dot = $(event.target);
-      selectedLi = dot.parent();
-      ul = selectedLi.parent();
-      currentLi = ul.find('.current');
-      if (selectedLi.is(currentLi)) {
-        return;
-      }
-      if (currentLi) {
-        currentLi.removeClass('current');
-        currentLi.find('.tooltip').css({
-          visibility: 'hidden',
-          opacity: 0
-        });
-      }
-      selectedLi.addClass('current');
-      if (dot.attr('href')) {
-        selectedLi.removeClass('current');
-        return;
-      }
-    };
-
-    $('.tooltip-target').hover(tooltipTargetHover);
-    $('.tooltip-target').mouseleave(tooltipTargetHoverOut);
-    $('.tooltip-target').click(tooltipTargetClick);
-  });
-
-}).call(this);
+  $('.tooltip-target').hover(tooltipTargetHover);
+  $('.tooltip-target').mouseleave(tooltipTargetHoverOut);
+  $('.tooltip-target').click(tooltipTargetClick);
+});
