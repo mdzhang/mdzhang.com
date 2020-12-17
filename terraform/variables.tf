eabface43a15179c04681c0a90b7b60a7a6176ad
Michelle D Zhang <zhang.michelle.d@gmail.com>
2020-12-17 10:03:42 -0600

    tf

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "us-west-1"
+}
+
+variable "aws_dns_ns" {
+  default = [
+    "ns-181.awsdns-22.com.",
+    "ns-1972.awsdns-54.co.uk.",
+    "ns-809.awsdns-37.net.",
+    "ns-1066.awsdns-05.org."
+  ]
+}
+
+variable "domain" {
+  default = "mdzhang.com"
+}
