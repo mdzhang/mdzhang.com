57a130cbc5ad24a29c3499e7c603110ab7432526
meesh 🤘 <meesh@found.com>
2023-08-22 09:18:29 -0400

    add dep
    https://www.sanity.io/plugins/sanity-astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,18 +1,30 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
+import sanity from "@sanity/astro";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   // used to generate images
   site: `http://${process.env.BASEURL}/`,
   trailingSlash: 'ignore',
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [
+    sitemap(),
+    UnoCSS({
+      injectReset: true
+    }),
+    sanity({
+      projectId: "TODO",
+      dataset: "next",
+      useCdn: true,
+    }),
+  ],
   vite: {
     optimizeDeps: {
-      exclude: ['@resvg/resvg-js'],
-    },
-  },
+      exclude: ['@resvg/resvg-js']
+    }
+  }
 });
