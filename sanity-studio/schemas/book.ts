8772d5574b5da8e7709ff8b10766d3f0dd5e620f
meesh 🤘 <meesh@found.com>
2023-08-22 09:30:42 -0400

    add a book schema

---
+++ sanity-studio/schemas/book.ts
@@ -0,0 +1,60 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'book',
+  title: 'Book',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+    }),
+    defineField({
+      name: 'isbn',
+      title: 'ISBN',
+      type: 'string',
+    }),
+    defineField({
+      name: 'isbn13',
+      title: 'ISBN13',
+      type: 'string',
+    }),
+    defineField({
+      name: 'publisher',
+      title: 'Publisher',
+      type: 'string',
+    }),
+    defineField({
+      name: 'readAt',
+      title: 'Read At',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'publicationYear',
+      title: 'Publication Year',
+      type: 'number',
+    }),
+    defineField({
+      name: 'myRating',
+      title: 'My Rating',
+      type: 'number',
+    }),
+    defineField({
+      name: 'shelves',
+      title: 'Shelves',
+      type: 'array',
+      of: [
+        {
+          title: 'Shelf',
+          type: 'string',
+        },
+      ],
+    }),
+  ],
+})
